e63a8a28b009c9ce428d5d5407220996120be40e
sandesh <sandeshb981@gmail.com>
2019-06-27 22:52:18 +0200

    Initial commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,158 @@
+<footer class="footer bg-light section-pattern footer-bg"
+        data-bg-img="img/section-pattern/footer-pattern.png">
+    <!-- Footer Top Begin -->
+    <div class="footer-top pt-60">
+        <div class="container border-bottom">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <!-- Contact Widget Begin -->
+                    <div class="widget widget_contact_info">
+                        <!-- Widget Logo Begin -->
+                        <div class="widget-logo">
+                            <img src="img/footer_logo.png" data-rjs="2" alt="">
+                        </div>
+                        <!-- Widget Logo End -->
+
+                        <!-- Widget Content Begin -->
+                        <div class="info-content">
+                            <div class="single-info">
+                                <span>Office Location</span>
+                                <p>173 Collins Street West victoria, Melbourne, Australia</p>
+                            </div>
+                            <div class="single-info">
+                                <span>Business Phone</span>
+                                <p><a href="#">+0096665431</a><a href="#">+0096667331</a></p>
+                            </div>
+                            <div class="single-info">
+                                <span>Support mail</span>
+                                <p>
+                                    <a href="#">bizidea@info.com</a>
+                                    <a href="#">bizidea@gmail.com</a>
+                                </p>
+                            </div>
+                        </div>
+                        <!-- Widget Content End -->
+                    </div>
+                    <!-- About Widget End -->
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <!-- Widget Recent Post Begin -->
+                    <div class="widget widget_recent_entries">
+                        <!-- Widget Title Begin  -->
+                        <div class="widget-title">
+                            <h4>Recent Posts</h4>
+                        </div>
+                        <!-- Widget Title End  -->
+
+                        <!-- Single Latest Post Begin -->
+                        <div class="single-post media">
+                            <div class="post-image">
+                                <img src="img/blog/recent-post.png" data-rjs="2" alt="">
+                            </div>
+                            <div class="post-content media-body">
+                                <span class="posted-on">18 Feb, 2019</span>
+                                <h5><a href="#">Funds in Institutional Portfolios</a></h5>
+                            </div>
+                        </div>
+                        <!-- Single Latest Post End -->
+
+                        <!-- Single Latest Post Begin -->
+                        <div class="single-post media">
+                            <div class="post-image">
+                                <img src="img/blog/recent-post.png" data-rjs="2" alt="">
+                            </div>
+                            <div class="post-content media-body">
+                                <span class="posted-on">18 Feb, 2019</span>
+                                <h5><a href="#">Funds in Institutional Portfolios</a></h5>
+                            </div>
+                        </div>
+                        <!-- Single Latest Post End -->
+                    </div>
+                    <!-- Widget Recent Post End -->
+                </div>
+                <div class="col-lg-2 col-sm-6">
+                    <!-- Widget Quick Nav -->
+                    <div class="widget widget_nav_menu">
+                        <!-- Widget Title Begin  -->
+                        <div class="widget-title">
+                            <h4>Quick Links</h4>
+                        </div>
+                        <!-- Widget Title End  -->
+
+                        <!-- Menu Begin -->
+                        <ul class="menu">
+                            <li><a href="#">About Company</a></li>
+                            <li><a href="#">Our Services</a></li>
+                            <li><a href="#">Our Experts</a></li>
+                            <li><a href="#">Get Constultation</a></li>
+                            <li><a href="#">Terms & Condition</a></li>
+                            <li><a href="#">Privacy policy</a></li>
+                        </ul>
+                        <!-- Menu End -->
+                    </div>
+                    <!-- Widget Quick Nav -->
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <!-- Widget Newsletter Begin -->
+                    <div class="widget widget_newsletter">
+                        <!-- Widget Title Begin  -->
+                        <div class="widget-title">
+                            <h4>Newsletter</h4>
+                        </div>
+                        <!-- Widget Title End  -->
+
+                        <div class="newsletter-content">
+                            <P>Cuteness you exquisite ourselves now end
+                                forfeited. Enquire ye without it garrets himself. Interest our nor received followed
+                                was.</P>
+
+                            <form action="https://themelooks.us13.list-manage.com/subscribe/post?u=79f0b132ec25ee223bb41835f&amp;id=f4e0e93d1d" method="post" name="mc-embedded-subscribe-form" target="_blank" class="newsletter-form">
+                                <div class="theme-input-group">
+                                    <input type="text" placeholder="Your Email">
+                                    <button type="submit"><i class="fa fa-paper-plane-o"
+                                                             aria-hidden="true"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- Widget Newsletter End -->
+
+                    <!-- Widget Social Icon Begin -->
+                    <div class="widget widget_social_icon">
+                        <ul class="social_icon_list list-inline">
+                            <li>
+                                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- Widget Social Icon End -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Top End -->
+
+    <!-- Footer Bottom Begin -->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="copyright-text text-center">
+                        <span><a href="#">bizidea</a> &copy; Copyright 2019.All rights reserved.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Bottom End -->
+
+</footer>
---
+++ resources/views/layouts/preloader.blade.php
@@ -0,0 +1,5 @@
+<div class='preloader w-100 h-100 position-fixed'>
+    <div class="loader">
+        <img class="icon" src="assets/img/preloader.png" alt="">
+    </div>
+</div>
