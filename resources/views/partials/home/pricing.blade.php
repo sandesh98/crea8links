7cc704c7e598994bb51e2ddefca97345b07d8e50
sandesh <sandeshb981@gmail.com>
2019-06-30 23:03:38 +0200

    Partials created

---
+++ resources/views/partials/home/pricing.blade.php
@@ -0,0 +1,251 @@
+<!-- Pricing Plan Begin -->
+<section class="pt-120 pb-120 section-pattern" data-bg-img="img/section-pattern/price-pattern.png">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <!-- Section Title Begin -->
+                <div class="section-title text-center">
+                    <h3>Pricing</h3>
+                    <h2>Choose Your Best Plan</h2>
+                    <p>On recommend tolerably my belonging or am. Mutual has cannot beauty indeed now sussex merely
+                        you. It possible no
+                        husbands jennings ye offended packages pleasant he.</p>
+                </div>
+                <!-- Section Title End -->
+            </div>
+        </div>
+
+        <!-- Pricing NavTab Begin -->
+        <div class="pricing-navtab">
+            <!-- Pricing Nav Begin -->
+            <ul class="row nav nav-tabs" role="tablist">
+                <!-- Single Nav Begin -->
+                <li class="nav-item col-lg-3 col-sm-6">
+                    <a href="#personal" class="nav-link text-center active" data-toggle="tab" role="tab">
+                        <h4>Personal</h4>
+                        <h2>$250</h2>
+                    </a>
+                </li>
+
+                <!-- Single Nav End -->
+
+                <!-- Single Nav Begin -->
+                <li class="nav-item col-lg-3 col-sm-6">
+                    <a href="#startup" class="nav-link text-center" data-toggle="tab" role="tab">
+                        <h4>Startup</h4>
+                        <h2>$500</h2>
+                    </a>
+                </li>
+                <!-- Single Nav End -->
+
+                <!-- Single Nav Begin -->
+                <li class="nav-item col-lg-3 col-sm-6">
+                    <a href="#business" class="nav-link text-center" data-toggle="tab" role="tab">
+                        <h4>Business</h4>
+                        <h2>$1050</h2>
+                    </a>
+                </li>
+                <!-- Single Nav End -->
+
+                <!-- Single Nav Begin -->
+                <li class="nav-item col-lg-3 col-sm-6">
+                    <a href="#entrepreneur" class="nav-link text-center" data-toggle="tab" role="tab">
+                        <h4>Entrepreneur</h4>
+                        <h2>$3000</h2>
+                    </a>
+                </li>
+                <!-- Single Nav End -->
+
+            </ul>
+            <!-- Pricing Nav End -->
+
+            <!-- Pricing Tab Content Begin -->
+            <div class="tab-content">
+                <!-- Single Content Begin -->
+                <div class="tab-pane fadeInUp animated show active" id="personal" role="tabpanel">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <!-- Tab Pane Text Begin -->
+                            <div class="tab-pane-text">
+                                <h3>What’s included in this package</h3>
+                                <ul class="list-unstyled list-check">
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Market sizing and share
+                                        analysis</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Product value proposition
+                                        analysis</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Identify strategic
+                                        partnership</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Implementation milestone
+                                        review</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Innovation opportunities</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Distribution channel
+                                        opportunities</li>
+                                </ul>
+                            </div>
+                            <!-- Tab Pane Text End -->
+                        </div>
+                        <div class="col-lg-6">
+                            <!-- Tab Pane Text Begin -->
+                            <div class="tab-pane-text mt-50 mt-lg-0">
+                                <h3>Business approach -</h3>
+                                <p>Our experts follow the business strategy to grow up your business and engage more
+                                    customers to your door.</p>
+                                <ul class="list-unstyled list-check">
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Getting to know your business
+                                        vision and the key strategic drivers.</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Detailed research of the
+                                        business environment, target market, and marketing as well as growth
+                                        opportunities.</li>
+                                </ul>
+                                <a href="#" class="btn"><span>purchase</span></a>
+                            </div>
+                            <!-- Tab Pane Text End -->
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Content End -->
+
+                <!-- Single Content Begin -->
+                <div class="tab-pane fadeInUp animated" id="startup" role="tabpanel">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <!-- Tab Pane Text Begin -->
+                            <div class="tab-pane-text">
+                                <h3>What’s included in this package</h3>
+                                <ul class="list-unstyled list-check">
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Market sizing and share
+                                        analysis</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Product value proposition
+                                        analysis</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Identify strategic
+                                        partnership</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Implementation milestone
+                                        review</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Innovation opportunities</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Distribution channel
+                                        opportunities</li>
+                                </ul>
+                            </div>
+                            <!-- Tab Pane Text End -->
+                        </div>
+                        <div class="col-lg-6">
+                            <!-- Tab Pane Text Begin -->
+                            <div class="tab-pane-text mt-50 mt-lg-0">
+                                <h3>Business approach -</h3>
+                                <p>Our experts follow the business strategy to grow up your business and engage more
+                                    customers to your door.
+                                </p>
+                                <ul class="list-unstyled list-check">
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Getting to know your business
+                                        vision and the key
+                                        strategic drivers.</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Detailed research of the
+                                        business environment, target
+                                        market, and marketing as well as growth opportunities.</li>
+                                </ul>
+                                <a href="#" class="btn"><span>purchase</span></a>
+                            </div>
+                            <!-- Tab Pane Text End -->
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Content End -->
+
+                <!-- Single Content Begin -->
+                <div class="tab-pane fadeInUp animated" id="business" role="tabpanel">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <!-- Tab Pane Text Begin -->
+                            <div class="tab-pane-text">
+                                <h3>What’s included in this package</h3>
+                                <ul class="list-unstyled list-check">
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Market sizing and share
+                                        analysis</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Product value proposition
+                                        analysis</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Identify strategic
+                                        partnership</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Implementation milestone
+                                        review</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Innovation opportunities</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Distribution channel
+                                        opportunities</li>
+                                </ul>
+                            </div>
+                            <!-- Tab Pane Text End -->
+                        </div>
+                        <div class="col-lg-6">
+                            <!-- Tab Pane Text Begin -->
+                            <div class="tab-pane-text mt-50 mt-lg-0">
+                                <h3>Business approach -</h3>
+                                <p>Our experts follow the business strategy to grow up your business and engage more
+                                    customers to your door.
+                                </p>
+                                <ul class="list-unstyled list-check">
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Getting to know your business
+                                        vision and the key
+                                        strategic drivers.</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Detailed research of the
+                                        business environment, target
+                                        market, and marketing as well as growth opportunities.</li>
+                                </ul>
+                                <a href="#" class="btn"><span>purchase</span></a>
+                            </div>
+                            <!-- Tab Pane Text End -->
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Content End -->
+
+                <!-- Single Content Begin -->
+                <div class="tab-pane fadeInUp animated" id="entrepreneur" role="tabpanel">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <!-- Tab Pane Text Begin -->
+                            <div class="tab-pane-text">
+                                <h3>What’s included in this package</h3>
+                                <ul class="list-unstyled list-check">
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Market sizing and share
+                                        analysis</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Product value proposition
+                                        analysis</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Identify strategic
+                                        partnership</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Implementation milestone
+                                        review</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Innovation opportunities</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Distribution channel
+                                        opportunities</li>
+                                </ul>
+                            </div>
+                            <!-- Tab Pane Text End -->
+                        </div>
+                        <div class="col-lg-6">
+                            <!-- Tab Pane Text Begin -->
+                            <div class="tab-pane-text mt-50 mt-lg-0">
+                                <h3>Business approach -</h3>
+                                <p>Our experts follow the business strategy to grow up your business and engage more
+                                    customers to your door.
+                                </p>
+                                <ul class="list-unstyled list-check">
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Getting to know your business
+                                        vision and the key
+                                        strategic drivers.</li>
+                                    <li><i class="fa fa-check" aria-hidden="true"></i> Detailed research of the
+                                        business environment, target
+                                        market, and marketing as well as growth opportunities.</li>
+                                </ul>
+                                <a href="#" class="btn"><span>purchase</span></a>
+                            </div>
+                            <!-- Tab Pane Text End -->
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Content End -->
+            </div>
+            <!-- Pricing Tab Content End -->
+        </div>
+        <!-- Pricing NavTab End -->
+    </div>
+</section>
+<!-- Pricing Plan End -->
---
+++ resources/views/partials/home/team.blade.php
@@ -0,0 +1,253 @@
+<section class="pt-120 pb-70 section-pattern" data-bg-img="img/section-pattern/testimonial-pattern.png">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <!-- Section Title Begin -->
+                <div class="section-title text-center">
+                    <h3>Team Members</h3>
+                    <h2>Our Experts Team Members</h2>
+                    <p>Ferrars all spirits his imagine effects amongst neither. It bachelor cheerful of mistaken. Tore
+                        has sons put upon wife
+                        use bred seen. Its dissimilar invitation ten has discretion unreserved.</p>
+                </div>
+                <!-- Section Title End -->
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+                <!-- Single Team Begin -->
+                <div class="single-team-member">
+                    <!-- Team Image Begin -->
+                    <div class="image position-relative">
+                        <img src="img/shapes/team.svg" class="svg" alt="">
+                        <img src="img/team/team-1.jpg" data-rjs="2" alt="">
+                    </div>
+                    <!-- Team Image End -->
+
+                    <!-- Team Info Begin -->
+                    <div class="team-info">
+                        <div class="info-front text-center">
+                            <h4>Emillie Cole</h4>
+                            <p>CEO of Bizidea</p>
+                        </div>
+
+                        <div class="info-back">
+                            <ul class="social_icon_list list-inline text-center w-100 d-flex justify-content-around">
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Team Info End -->
+                </div>
+                <!-- Single Team End -->
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <!-- Single Team Begin -->
+                <div class="single-team-member">
+                    <!-- Team Image Begin -->
+                    <div class="image position-relative">
+                        <img src="img/shapes/team.svg" class="svg" alt="">
+                        <img src="img/team/team-2.jpg" data-rjs="2" alt="">
+                    </div>
+                    <!-- Team Image End -->
+
+                    <!-- Team Info Begin -->
+                    <div class="team-info">
+                        <div class="info-front text-center">
+                            <h4>Jack Reader</h4>
+                            <p>CEO of Bizidea</p>
+                        </div>
+
+                        <div class="info-back">
+                            <ul class="social_icon_list list-inline text-center w-100 d-flex justify-content-around">
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Team Info End -->
+
+                </div>
+                <!-- Single Team End -->
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <!-- Single Team Begin -->
+                <div class="single-team-member">
+                    <!-- Team Image Begin -->
+                    <div class="image position-relative">
+                        <img src="img/shapes/team.svg" class="svg" alt="">
+                        <img src="img/team/team-3.jpg" data-rjs="2" alt="">
+                    </div>
+                    <!-- Team Image End -->
+
+                    <!-- Team Info Begin -->
+                    <div class="team-info">
+                        <div class="info-front text-center">
+                            <h4>Mickle Jection</h4>
+                            <p>CEO of Bizidea</p>
+                        </div>
+
+                        <div class="info-back">
+                            <ul class="social_icon_list list-inline text-center w-100 d-flex justify-content-around">
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Team Info End -->
+
+                </div>
+                <!-- Single Team End -->
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <!-- Single Team Begin -->
+                <div class="single-team-member">
+                    <!-- Team Image Begin -->
+                    <div class="image position-relative">
+                        <img src="img/shapes/team.svg" class="svg" alt="">
+                        <img src="img/team/team-4.jpg" data-rjs="2" alt="">
+                    </div>
+                    <!-- Team Image End -->
+
+                    <!-- Team Info Begin -->
+                    <div class="team-info">
+                        <div class="info-front text-center">
+                            <h4>Jeson Roy</h4>
+                            <p>CEO of Bizidea</p>
+                        </div>
+
+                        <div class="info-back">
+                            <ul class="social_icon_list list-inline text-center w-100 d-flex justify-content-around">
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Team Info End -->
+
+                </div>
+                <!-- Single Team End -->
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <!-- Single Team Begin -->
+                <div class="single-team-member">
+                    <!-- Team Image Begin -->
+                    <div class="image position-relative">
+                        <img src="img/shapes/team.svg" class="svg" alt="">
+                        <img src="img/team/team-5.jpg" data-rjs="2" alt="">
+                    </div>
+                    <!-- Team Image End -->
+
+                    <!-- Team Info Begin -->
+                    <div class="team-info">
+                        <div class="info-front text-center">
+                            <h4>Andy Mare</h4>
+                            <p>CEO of Bizidea</p>
+                        </div>
+
+                        <div class="info-back">
+                            <ul class="social_icon_list list-inline text-center w-100 d-flex justify-content-around">
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Team Info End -->
+
+                </div>
+                <!-- Single Team End -->
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <!-- Single Team Begin -->
+                <div class="single-team-member">
+                    <!-- Team Image Begin -->
+                    <div class="image position-relative">
+                        <img src="img/shapes/team.svg" class="svg" alt="">
+                        <img src="img/team/team-6.jpg" data-rjs="2" alt="">
+                    </div>
+                    <!-- Team Image End -->
+
+                    <!-- Team Info Begin -->
+                    <div class="team-info">
+                        <div class="info-front text-center">
+                            <h4>John Snow</h4>
+                            <p>CEO of Bizidea</p>
+                        </div>
+
+                        <div class="info-back">
+                            <ul class="social_icon_list list-inline text-center w-100 d-flex justify-content-around">
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Team Info End -->
+
+                </div>
+                <!-- Single Team End -->
+            </div>
+        </div>
+    </div>
+</section>
